88352ca87f7351037e816fbbca4eb972cade1582
Clayton-Klemm <36232582+Clayton-Klemm@users.noreply.github.com>
2023-10-28 10:39:25 -0500

    added a few more comments

--- pokemonYellowGambling.cpp
+++ pokemonYellowGambling.cpp
@@ -19,17 +19,16 @@ enum States {
 // to store our templates (the bar regions)
 std::map<std::string, cv::Mat> templates;
 
-// function for simulating a key press in windows
+// Function for simulating a key press in Windows.
 void SimulateKeyPress(char key) {
-	INPUT input[2]; // INPUT is a type of struct used by SendInput to store information 
-	// for synthesizing input events such as keystrokes, mouse movement, and mouse clicks
-	// MOUSEINPUT mi, KEYBDINPUT ki, HARDWAREINPUT hi, 
-
+	INPUT input[2]; // INPUT is a struct used by SendInput to store information 
+                    // for synthesizing input events (keystrokes, mouse movement, etc.)
+	
 	// Set up the INPUT for key down
 	input[0].type = INPUT_KEYBOARD;
-	input[0].ki.wVk = VkKeyScan(key); // Virtual key code
-	input[0].ki.wScan = MapVirtualKey(input[0].ki.wVk, 0);
-	input[0].ki.dwFlags = 0; // Key down
+	input[0].ki.wVk = VkKeyScan(key); // Retrieves the virtual-key code and OEM scan code for a given key.
+	input[0].ki.wScan = MapVirtualKey(input[0].ki.wVk, 0); // Translates virtual-key code into a scan code.
+	input[0].ki.dwFlags = 0; // Indicates a key down event.
 	input[0].ki.time = 0;
 	input[0].ki.dwExtraInfo = 0;
 	// Set up the INPUT for key up
@@ -41,58 +40,81 @@ void SimulateKeyPress(char key) {
 	input[1].ki.dwExtraInfo = 0;
 	// Send the input
 	SendInput(1, &input[0], sizeof(INPUT)); // Key down
-	Sleep(50); // Sleep for 50 milliseconds
+	Sleep(50); // Introduce a short delay to simulate the duration of a key press. In this case 50 milliseconds.
 	SendInput(1, &input[1], sizeof(INPUT)); // Key up
 }
 
-// function for capturing a screenshot of our region of interest
+// Function to capture a screenshot of a specified region.
+// Parameters:
+// - region: The rectangle defining the region to capture.
+// - hwnd: Handle to the window we are capturing from.
 cv::Mat capture_region(const cv::Rect& region, HWND hwnd) {
-	// with and height of the capture region
+	// Retrieve width and height from the specified capture region.
 	int width = region.width;
 	int height = region.height;
+
+	// Create a cv::Mat (an image container) to hold the screenshot data.
 	cv::Mat screenshot(height, width, CV_8UC4);
+
+	// Get the device context for the specified window (hwnd).
 	HDC hdcWindow = GetDC(hwnd);
 	if (!hdcWindow) {
 		std::cerr << "Failed to get device context!" << std::endl;
-		return screenshot; // Return empty screenshot
+		return screenshot; // If failed, return an empty screenshot.
 	}
+
+	// Create a memory device context that is compatible with the window's device context.
 	HDC hdcMemory = CreateCompatibleDC(hdcWindow);
 	if (!hdcMemory) {
 		std::cerr << "Failed to create compatible device context!" << std::endl;
 		ReleaseDC(hwnd, hdcWindow);
-		return screenshot; // Return empty screenshot
+		return screenshot; // If failed, return an empty screenshot and release acquired resources.
 	}
+
+	// Create a bitmap compatible with the window's device context to hold the screenshot.
 	HBITMAP hBitmap = CreateCompatibleBitmap(hdcWindow, width, height);
 	if (!hBitmap) {
 		std::cerr << "Failed to create compatible bitmap!" << std::endl;
-		DeleteDC(hdcMemory);
-		ReleaseDC(hwnd, hdcWindow);
-		return screenshot; // Return empty screenshot
+		DeleteDC(hdcMemory); // Release the memory device context.
+		ReleaseDC(hwnd, hdcWindow); // Release the window's device context.
+		return screenshot; // If failed, return an empty screenshot and release acquired resources.
 	}
+
+	// Select the new bitmap into the memory device context.
 	HBITMAP hOldBitmap = (HBITMAP)SelectObject(hdcMemory, hBitmap);
 	if (!hOldBitmap) {
 		std::cerr << "Failed to select object into memory device context!" << std::endl;
-		DeleteObject(hBitmap);
-		DeleteDC(hdcMemory);
-		ReleaseDC(hwnd, hdcWindow);
-		return screenshot; // Return empty screenshot
+		DeleteObject(hBitmap); // Delete the bitmap.
+		DeleteDC(hdcMemory); // Release the memory device context.
+		ReleaseDC(hwnd, hdcWindow); // Release the window's device context.
+		return screenshot; // If failed, return an empty screenshot and release acquired resources.
 	}
+
+	// Copy the specified region from the window's device context to the memory device context.
 	BitBlt(hdcMemory, 0, 0, width, height, hdcWindow, region.x, region.y, SRCCOPY);
+
+	// Initialize the BITMAPINFOHEADER structure for retrieving the bitmap data.
 	BITMAPINFOHEADER bi = { 0 };
 	bi.biSize = sizeof(BITMAPINFOHEADER);
 	bi.biWidth = width;
-	bi.biHeight = -height;
+	bi.biHeight = -height; // Negative height indicates top-down orientation.
 	bi.biPlanes = 1;
-	bi.biBitCount = 32;
-	bi.biCompression = BI_RGB;
+	bi.biBitCount = 32; // 32 bits-per-pixel.
+	bi.biCompression = BI_RGB; // No compression.
+
+	// Retrieve the bitmap data and store it in the cv::Mat container.
 	GetDIBits(hdcMemory, hBitmap, 0, height, screenshot.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
-	// Clean up resources here
+
+	// Clean up: Restore the old bitmap, delete created objects, and release device contexts.
 	SelectObject(hdcMemory, hOldBitmap);
 	DeleteObject(hBitmap);
 	DeleteDC(hdcMemory);
 	ReleaseDC(hwnd, hdcWindow);
-	return screenshot;
+
+	return screenshot; // Return the captured screenshot.
 }
+
+
 struct RegionData {
 	cv::Rect roi;
 	cv::Mat templateImg;
@@ -106,7 +128,7 @@ bool check_template(const RegionData& regionData, HWND hwnd) {
 	cv::matchTemplate(captured, regionData.templateImg, result, cv::TM_SQDIFF_NORMED);
 	double minVal;
 	cv::minMaxLoc(result, &minVal, nullptr, nullptr, nullptr);
-	return minVal < .1;
+	return minVal < .05;
 }
 
 // Modified check_state function to use the RegionData
